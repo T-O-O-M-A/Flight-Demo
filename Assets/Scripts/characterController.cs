63edf082c9c496a9b7b3874403a00757c6d01a88
hajnina <hajnina11@gmail.com>
2016-06-01 21:27:19 +0200

    0.04
    postavička moc nefunguje.... tohle snad pomuze:
    http://answers.unity3d.com/questions/1150967/tilt-character-with-acceleration.html

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class characterController : MonoBehaviour {
 
+    public Rigidbody player;
+    public Camera kamera;
+    Vector3 rychlost;
 	// Use this for initialization
 	void Start () {
-	
+        rychlost = Vector3.zero;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        //player.transform.rotation= Quaternion.Euler(player.velocity - rychlost);
+        rychlost = player.velocity;
+        player.AddRelativeForce(Input.GetAxis("Horizontal")*force,0, Input.GetAxis("Vertical")*force);
+    }
 }
