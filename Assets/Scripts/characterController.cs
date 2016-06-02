731cfd43512bb1d69cfae3ba690b1e5732044fc4
hajnina <hajnina11@gmail.com>
2016-06-02 21:39:14 +0200

    0.05 - Turbulence
    Added turbulence. Inertia based character tilt still in works....
    turbulence still needs some tuning...

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -6,7 +6,7 @@ public class characterController : MonoBehaviour {
     public Rigidbody player;
     public Camera kamera;
     float force = 50;
-    Vector3 rychlost;
+    Vector3 rychlost, staraRychlost, zrychleni;
 	// Use this for initialization
 	void Start () {
         rychlost = Vector3.zero;
@@ -14,10 +14,20 @@ public class characterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        //player.transform.rotation= Quaternion.Euler(player.velocity - rychlost);
+        staraRychlost = rychlost;
         rychlost = player.velocity;
+        zrychleni = staraRychlost - rychlost;
+        //Debug.Log(zrychleni.magnitude/Time.deltaTime);
+        zrychleni.z -= 9.81f;
+        //player.transform.rotation= Quaternion.Euler(player.velocity - rychlost);
+        /*rychlost = player.velocity;
         player.AddRelativeForce(Input.GetAxis("Horizontal")*force,0, Input.GetAxis("Vertical")*force);
         kamera.transform.rotation = Quaternion.Lerp(player.rotation, Quaternion.Euler(-Input.mousePosition.y,Input.mousePosition.x,0), Time.deltaTime);
-        player.transform.up = player.velocity - rychlost;
+        player.transform.up = player.velocity - rychlost;*/
+
+        transform.rotation = Quaternion.LookRotation(transform.position,transform.position-zrychleni);
+        //player.transform.rotation = Quaternion.FromToRotation(transform.position, transform.position - zrychleni);
+     
+
     }
 }
