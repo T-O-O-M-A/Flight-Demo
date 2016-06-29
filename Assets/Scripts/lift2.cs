af752e1a91896a4b04fd76cc492ccda7652c4800
hajnina <hajnina11@gmail.com>
2016-06-29 22:16:03 +0200

    0.07 - Angle of Attack

--- Assets/Scripts/lift2.cs
+++ Assets/Scripts/lift2.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class lift2 : MonoBehaviour {
 
+    float uhelNabehu, sila;
+    public float plocha, koeficient;
+    Rigidbody kridlo;
+
 	// Use this for initialization
 	void Start () {
-	
+        kridlo = this.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        //uhelNabehu = Mathf.Atan(transform.InverseTransformDirection(kridlo.velocity).y / Mathf.Sqrt(transform.InverseTransformDirection(kridlo.velocity).x * transform.InverseTransformDirection(kridlo.velocity).x + transform.InverseTransformDirection(kridlo.velocity).z * transform.InverseTransformDirection(kridlo.velocity).z));
+        //CoL *= Mathf.Sign(transform.InverseTransformDirection(kridlo.velocity).z);
+        uhelNabehu = -Mathf.Atan(transform.InverseTransformDirection(kridlo.velocity).y / transform.InverseTransformDirection(kridlo.velocity).z);
+        uhelNabehu *= Mathf.Rad2Deg;
+        sila= vztlak(uhelNabehu);
+        Debug.Log(sila);
+        kridlo.AddRelativeForce(0, sila, 0);
+    }
+
+    float vztlak(float AoA) {
+        float maxAngle = 30, rychlostOdtrzeni = 3;
+        if (Mathf.Abs(AoA) < maxAngle)
+        {
+            return AoA * koeficient * plocha;
+        }else
+        {
+            float x = AoA - rychlostOdtrzeni*Mathf.Abs(maxAngle - Mathf.Abs(AoA))*Mathf.Sign(AoA);
+            if (Mathf.Sign(x)!=Mathf.Sign(AoA)) { x = 0; }
+            return x*koeficient*plocha;
+        }
+    }
 }
---
+++ Assets/Scripts/proceduralTerrain.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class proceduralTerrain : MonoBehaviour {
+
+    TerrainData terrain;
+    public int velikost;
+	// Use this for initialization
+	void Start () {
+        terrain = this.GetComponent<TerrainData>();
+        float[,] vysky = new float[velikost, velikost];
+        for (int x = 0; x < velikost; x++)
+        {
+            for(int y=0; y<velikost; y++)
+            {
+                vysky[y, x] = Mathf.PerlinNoise(y/10, x/10);
+                Debug.Log(vysky[y, x]);
+            }
+        }
+        terrain.SetHeights(0, 0, vysky);
+        terrain.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
